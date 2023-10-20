48f9a6301926c6a8fbcfb448dd09fb439f0de913
Konstantin Chernenko <konstantin032005@gmail.com>
2023-10-21 01:42:50 +0300

    Добавил вторую кнопку

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,12 @@ namespace WinFormsApp1
         {
             label1.Text = "Я пидарас";
             int a = 0;
-            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Я пидарас, а не русский";
         }
     }
 }
