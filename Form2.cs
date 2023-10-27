a2af979e42559d3f8878263b383495eddb89b464
Константин Черненко <konstantin032005@gmail.com>
2023-10-27 19:10:56 +0300

    Добавили фон на вторую старницу

--- Form2.cs
+++ Form2.cs
@@ -21,5 +21,10 @@ namespace WinFormsApp1
         {
             this.Close();
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
