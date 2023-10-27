c40dbd71754ddf2c70534260c769e91cd45fc05d
Константин Черненко <konstantin032005@gmail.com>
2023-10-27 18:36:48 +0300

    Добавили общее офомление

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,32 +29,49 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(90, 112);
+            button1.BackgroundImage = Properties.Resources._07aba1377ac5c14;
+            button1.Location = new Point(55, 54);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(171, 29);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "<----------";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.d0e0e259bf0aba4da742bedff1d4b8a5;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1000, 600);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.WaitOnLoad = true;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1902, 1033);
+            ClientSize = new Size(1000, 600);
             Controls.Add(button1);
+            Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private PictureBox pictureBox1;
     }
 }
