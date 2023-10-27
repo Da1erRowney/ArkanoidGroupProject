0bc91ac8939ff823569a21861c0c4c37d8cea4e8
Konstantin Chernenko <konstantin032005@gmail.com>
2023-10-27 23:51:50 +0300

    Добавил чтото

--- Form1.cs
+++ Form1.cs
@@ -12,25 +12,11 @@ namespace WinFormsApp1
         {
             Form2 form2 = new Form2();
 
-            this.Hide();
-            form2.Show();
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //fdf
-            //Комментарий
             Form3 form3 = new Form3();
             this.Hide();
             form3.Show();
