7af9a6c30094a290729317d88a2bd5bb542ec14a
ASUS <ASUS@DESKTOP-0UJTF1L>
2023-10-30 18:40:08 +0300

    изменил стиль кнопок, переделал немного игру, добавил картинки для платформы, для шарика и для блоков.

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -37,34 +37,34 @@
             // 
             pictureBox1.Image = Properties.Resources.d0e0e259bf0aba4da742bedff1d4b8a5;
             pictureBox1.Location = new Point(0, -1);
-            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(87, 37);
+            pictureBox1.Size = new Size(1131, 774);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.WaitOnLoad = true;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
             // 
             // button1
             // 
-            button1.Location = new Point(59, 40);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.ForeColor = Color.Khaki;
+            button1.Location = new Point(67, 53);
             button1.Name = "button1";
-            button1.Size = new Size(82, 22);
+            button1.Size = new Size(94, 29);
             button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "<-----";
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click_1;
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1000, 600);
+            ClientSize = new Size(1143, 800);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form2";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form2";
--- Form2.cs
+++ Form2.cs
@@ -10,91 +10,131 @@ using System.Windows.Forms;
 using static System.Windows.Forms.AxHost;
 using Timer = System.Windows.Forms.Timer;
 
+
 namespace WinFormsApp1
 {
     public partial class Form2 : Form
     {
-        private Rectangle playerRectangle;
-        private Point playerSpeed;
-        private Point ballPosition;
-        private Point ballSpeed;
-        private int ballRadius;
-        private List<Rectangle> rectangles;
-        private List<Brush> rectangleBrushes;
-        private int rowCount; // Добавлено объявление переменной rowCount
-        private int rowLength; // Добавлено объявление переменной rowLength
-        private int rectangleWidth; // Добавлено объявление переменной rectangleWidth
-        private int rectangleHeight; // Добавлено объявление переменной rectangleHeight
-        private int spacing; // Добавлено объявление переменной spacing
-        private int startX; // Добавлено объявление переменной startX
-        private int startY; // Добавлено объявление переменной startY
-        private int gameAreaX; // Добавлено объявление переменной gameAreaX
-
+        private PictureBox paddle; // Платформа
+        private PictureBox ball; // Шарик
+        private PictureBox[] blocks; // Блоки
+        private Timer gameTimer;
+        private int ballSpeedX = 4; // Скорость шарика по горизонтали
+        private int ballSpeedY = 4; // Скорость шарика по вертикали
         public Form2()
         {
             InitializeComponent();
             InitializeGame();
         }
-
         private void InitializeGame()
         {
-            Size = new Size(1000, 900);
-            StartPosition = FormStartPosition.CenterScreen;
-            bool isGameOver = false;
+            // Создаем платформу
 
+            paddle = new PictureBox();
+            paddle.Image = Image.FromFile("../../../Resources/platform.png");
+            //paddle.Image = Properties.Resources.platform; // Замените "platform.png" на имя вашего изображения платформы в папке "Resources"
+            paddle.SizeMode = PictureBoxSizeMode.StretchImage;
+            paddle.Size = new Size(100, 40);
+            paddle.Location = new Point((ClientSize.Width - paddle.Width) / 2, ClientSize.Height - paddle.Height - 50);
+            // Устанавливаем прозрачный фон
+            paddle.BackColor = Color.Transparent;
+            Controls.Add(paddle);
+            paddle.BringToFront();
 
-            int gameAreaWidth = 600;
-            int gameAreaHeight = 500;
-            gameAreaX = (ClientSize.Width - gameAreaWidth) / 2;
-            int gameAreaY = (ClientSize.Height - gameAreaHeight) / 2;
+            // Создаем шарик
 
-            int playerX = gameAreaX + (gameAreaWidth - 100) / 2;
-            int playerY = gameAreaY + gameAreaHeight - 100;
-            int ballX = gameAreaX + gameAreaWidth / 2;
-            int ballY = playerY - 15;
+            ball = new PictureBox();
+            ball.Image = Image.FromFile("../../../Resources/ball.png"); // Замените "ball.png" на путь к вашей картинке шарика
+            ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            ball.Size = new Size(30, 30);
+            ball.Location = new Point((ClientSize.Width - ball.Width) / 2, paddle.Top - ball.Height);
+            ball.BackColor = Color.Transparent; // Устанавливаем прозрачный фон
+            Controls.Add(ball);
+            ball.BringToFront();
 
-            playerRectangle = new Rectangle(playerX, playerY, 100, 20);
-            playerSpeed = new Point(15, 0);
-
-            ballRadius = 10;
-            ballPosition = new Point(ballX, ballY);
-            ballSpeed = new Point(6, -6);
-
-            rectangles = new List<Rectangle>();
-            rectangleBrushes = new List<Brush>();
-
-            rowCount = 3;
-            rowLength = 5;
-            rectangleWidth = 80;
-            rectangleHeight = 60;
-            spacing = 5;
-            startX = gameAreaX + (gameAreaWidth - (rowLength * rectangleWidth + (rowLength - 1) * spacing)) / 2;
-            startY = gameAreaY + 20;
-
-            for (int row = 0; row < rowCount; row++)
+            // Создаем блоки
+            blocks = new PictureBox[30];
+            int blockIndex = 0;
+            int initialTop = 50;
+            int initialLeft = 30;
+            int blockWidth = 70;
+            int blockHeight = 30;
+            for (int row = 0; row < 3; row++)
             {
-                for (int col = 0; col < rowLength; col++)
+                for (int column = 0; column < 10; column++)
                 {
-                    int x = startX + (rectangleWidth + spacing) * col;
-                    int y = startY + (rectangleHeight + spacing) * row;
-
-                    Rectangle newRectangle = new Rectangle(x, y, rectangleWidth, rectangleHeight);
-                    Brush newBrush = Brushes.Red;
-
-                    rectangles.Add(newRectangle);
-                    rectangleBrushes.Add(newBrush);
+                    blocks[blockIndex] = new PictureBox();
+                    blocks[blockIndex].Image = Image.FromFile("../../../Resources/block2.png"); // Замените "block.png" на путь к вашей картинке блока
+                    blocks[blockIndex].SizeMode = PictureBoxSizeMode.StretchImage;
+                    blocks[blockIndex].Size = new Size(blockWidth, blockHeight);
+                    blocks[blockIndex].Location = new Point(initialLeft + column * (blockWidth + 10),
+                        initialTop + row * (blockHeight + 10));
+                    blocks[blockIndex].BackColor = Color.Transparent; // Устанавливаем прозрачный фон
+                    Controls.Add(blocks[blockIndex]);
+                    blocks[blockIndex].BringToFront();
+                    blockIndex++;
                 }
             }
 
-            DoubleBuffered = true;
-            KeyPreview = true;
-            KeyDown += Form2_KeyDown;
 
-            Timer timer = new Timer();
-            timer.Interval = 20;
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            // Создаем таймер для обновления игровых состояний
+            gameTimer = new Timer();
+            gameTimer.Interval = 1;
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
         }
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            // Двигаем шарик
+            ball.Left += ballSpeedX;
+            ball.Top += ballSpeedY;
+
+            // Проверяем столкновение шарика с платформой
+            if (ball.Bounds.IntersectsWith(paddle.Bounds))
+            {
+                ballSpeedY = -ballSpeedY;
+            }
+
+            // Проверяем столкновение шарика с блоками
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                if (ball.Bounds.IntersectsWith(blocks[i].Bounds) && blocks[i].Visible)
+                {
+                    blocks[i].Visible = false;
+                    ballSpeedY = -ballSpeedY;
+                    // Дополнительные действия при попадании шариком в блок
+                }
+            }
+
+            // Проверяем столкновение шарика со стенками
+            if (ball.Left <= 0 || ball.Right >= ClientSize.Width)
+            {
+                ballSpeedX = -ballSpeedX;
+            }
+            if (ball.Top <= 0)
+            {
+                ballSpeedY = -ballSpeedY;
+            }
+
+            // Проверяем, завершилась ли игра
+            if (ball.Top >= ClientSize.Height)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Игра окончена!");
+                Close();
+            }
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            // Перемещение платформы за курсором мыши
+            if (e.X >= paddle.Width / 2 && e.X <= ClientSize.Width - paddle.Width / 2)
+            {
+                paddle.Left = e.X - paddle.Width / 2;
+            }
+
+            base.OnMouseMove(e);
+        }
+
 
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -105,155 +145,14 @@ namespace WinFormsApp1
 
         }
 
-        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            int gameAreaWidth = 600; // Объявляем и инициализируем переменную gameAreaWidth
-
-            if (e.KeyCode == Keys.A)
+            if (e.X >= paddle.Width / 2 && e.X <= ClientSize.Width - paddle.Width / 2)
             {
-                if (playerRectangle.X - playerSpeed.X >= gameAreaX)
-                {
-                    playerRectangle.X -= playerSpeed.X;
-                }
-            }
-            else if (e.KeyCode == Keys.D)
-            {
-                if (playerRectangle.X + playerSpeed.X + playerRectangle.Width <= gameAreaX + gameAreaWidth)
-                {
-                    playerRectangle.X += playerSpeed.X;
-                }
-            }
-            else if (e.KeyCode == Keys.N)
-            {
-                Rectangle newRectangle = new Rectangle(150, 300, 80, 60);
-                Brush newBrush = Brushes.Green;
-
-                rectangles.Add(newRectangle);
-                rectangleBrushes.Add(newBrush);
-            }
-        }
-
-        private void ResetGame()
-        {
-            rectangles.Clear();
-            rectangleBrushes.Clear();
-
-            // Добавляем прямоугольники в список
-            for (int row = 0; row < rowCount; row++)
-            {
-                for (int col = 0; col < rowLength; col++)
-                {
-                    int x = startX + (rectangleWidth + spacing) * col;
-                    int y = startY + (rectangleHeight + spacing) * row;
-
-                    Rectangle newRectangle = new Rectangle(x, y, rectangleWidth, rectangleHeight);
-                    Brush newBrush = Brushes.Red;
-
-                    rectangles.Add(newRectangle);
-                    rectangleBrushes.Add(newBrush);
-                }
+                paddle.Left = e.X - paddle.Width / 2;
             }
 
-            ballPosition = new Point(playerRectangle.X + playerRectangle.Width / 2, playerRectangle.Y - ballRadius - 5); // Изменяем начальную позицию шарика
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            ballPosition.X += ballSpeed.X;
-            ballPosition.Y += ballSpeed.Y;
-
-            // Проверяем столкновение шарика с игроком
-            if (ballPosition.Y + ballRadius >= playerRectangle.Y && ballPosition.Y - ballRadius <= playerRectangle.Y + playerRectangle.Height)
-            {
-                if (ballPosition.X + ballRadius >= playerRectangle.X && ballPosition.X - ballRadius <= playerRectangle.X + playerRectangle.Width)
-                {
-                    ballSpeed.Y = -ballSpeed.Y;
-                }
-            }
-
-            // Проверяем столкновение шарика с прямоугольниками
-            for (int i = 0; i < rectangles.Count; i++)
-            {
-                Rectangle rectangle = rectangles[i];
-                if (ballPosition.Y + ballRadius >= rectangle.Y && ballPosition.Y - ballRadius <= rectangle.Y + rectangle.Height)
-                {
-                    if (ballPosition.X + ballRadius >= rectangle.X && ballPosition.X - ballRadius <= rectangle.X + rectangle.Width)
-                    {
-                        rectangles.RemoveAt(i);
-                        rectangleBrushes.RemoveAt(i);
-
-                        // Определяем, с какой стороны шарик столкнулся с прямоугольником
-                        bool collidedFromLeft = ballPosition.X + ballRadius <= rectangle.X && ballSpeed.X > 0;
-                        bool collidedFromRight = ballPosition.X - ballRadius >= rectangle.X + rectangle.Width && ballSpeed.X < 0;
-                        bool collidedFromBottom = ballPosition.Y - ballRadius <= rectangle.Y + rectangle.Height && ballSpeed.Y < 0;
-                        bool collidedFromTop = ballPosition.Y + ballRadius >= rectangle.Y && ballSpeed.Y > 0;
-
-                        if (collidedFromLeft || collidedFromRight)
-                        {
-                            // Меняем только горизонтальное направление движения
-                            ballSpeed.X = -ballSpeed.X;
-                        }
-                        else if (collidedFromBottom || collidedFromTop)
-                        {
-                            // Меняем только вертикальное направление движения
-                            ballSpeed.Y = -ballSpeed.Y;
-                        }
-                        else
-                        {
-                            // Меняем оба направления при столкновении с вертикальными сторонами прямоугольника
-                            ballSpeed.X = -ballSpeed.X;
-                            ballSpeed.Y = -ballSpeed.Y;
-                        }
-
-                        break;
-                    }
-                }
-            }
-
-            // Проверяем столкновение шарика с границами окна
-            if (ballPosition.X + ballRadius >= ClientSize.Width || ballPosition.X - ballRadius <= 0)
-            {
-                ballSpeed.X = -ballSpeed.X;
-            }
-            if (ballPosition.Y - ballRadius <= 0)
-            {
-                ballSpeed.Y = -ballSpeed.Y;
-            }
-            if (ballPosition.Y + ballRadius >= ClientSize.Height)
-            {
-                ResetGame();
-            }
-
-            Invalidate();
-        }
-
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-
-
-            // Рисуем фон области игры
-            int gameAreaWidth = 600;
-            int gameAreaHeight = 500;
-            int gameAreaX = (ClientSize.Width - gameAreaWidth) / 2;
-            int gameAreaY = (ClientSize.Height - gameAreaHeight) / 2;
-            g.FillRectangle(Brushes.White, gameAreaX, gameAreaY, gameAreaWidth, gameAreaHeight);
-
-            // Рисуем игрока
-            g.FillRectangle(Brushes.Blue, playerRectangle.X - gameAreaX, playerRectangle.Y - gameAreaY, playerRectangle.Width, playerRectangle.Height);
-
-            // Рисуем шарик
-            g.FillEllipse(Brushes.Black, ballPosition.X - ballRadius - gameAreaX, ballPosition.Y - ballRadius - gameAreaY, ballRadius * 2, ballRadius * 2);
-
-            // Рисуем прямоугольники
-            for (int i = 0; i < rectangles.Count; i++)
-            {
-                Rectangle rectangle = rectangles[i];
-                g.FillRectangle(rectangleBrushes[i], rectangle.X - gameAreaX, rectangle.Y - gameAreaY, rectangle.Width, rectangle.Height);
-            }
-
-            base.OnPaint(e);
+            base.OnMouseMove(e);
         }
     }
 }
